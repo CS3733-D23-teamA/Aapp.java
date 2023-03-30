ac66e1ab07c2c74bca136913f68a41c67eb4a137
ashleigh <amperez@wpi.edu>
2023-03-29 21:49:34 -0400

    Added attributes for ConferenceController

--- src/main/java/edu/wpi/teamA/controllers/ConferenceController.java
+++ src/main/java/edu/wpi/teamA/controllers/ConferenceController.java
@@ -4,23 +4,33 @@ import edu.wpi.teamA.navigation.Navigation;
 import edu.wpi.teamA.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class ConferenceController {
   @FXML MFXButton backButton;
   @FXML MFXButton submitButton;
   @FXML MFXButton clearButton;
-  String name_input;
+  @FXML TextField name_input;
+  String name;
+  int date;
+  int start;
+  int end;
+  int numPeople;
+  String additionalRequests;
 
   @FXML
   public void intitialize() {}
 
   public void goBack() {
+    // should be inherited through home page
     Navigation.navigate(Screen.HOME);
   }
 
   public void submitForm() {
     // send form information to database
-    System.out.println("Form submitted by: " + name_input);
+    name = name_input.getText();
+
+    System.out.println("Form submitted by: " + name);
     Navigation.navigate(Screen.SERVICE_REQUEST);
   }
 
